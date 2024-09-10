8dd516f17c3255f88f3668fb41a99f59404317c8
Juho Lehtimäki <juho.a.lehtimaki@gmail.com>
2024-09-10 23:37:00 +0300

    feat: add mouse wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,25 @@ import { CertificationsSection } from './sections/CertificationsSection';
 import { EducationSection } from './sections/EducationSection';
 import { Layout } from './components/Layout';
 import { AboutSection } from './sections/AboutSection';
+import { MouseWrapper } from './components/MouseWrapper';
 
 export const App: React.FC = () => (
-  <Layout>
-    <div className="flex flex-col lg:flex-row lg:gap-40">
-      <div className="lg:sticky lg:top-0 lg:h-screen">
-        <MeSection />
+  <MouseWrapper>
+    <Layout>
+      <div className="flex flex-col lg:flex-row lg:gap-40">
+        <div className="lg:sticky lg:top-0 lg:h-screen">
+          <MeSection />
+        </div>
+        <div className="flex flex-col">
+          <AboutSection />
+          <hr className="border-t border-gray-300 my-4" />
+          <ExperienceSection />
+          <hr className="border-t border-gray-300 my-4" />
+          <EducationSection />
+          <hr className="border-t border-gray-300 my-4" />
+          <CertificationsSection />
+        </div>
       </div>
-      <div className="flex flex-col">
-        <AboutSection />
-        <hr className="border-t border-gray-300 my-4" />
-        <ExperienceSection />
-        <hr className="border-t border-gray-300 my-4" />
-        <EducationSection />
-        <hr className="border-t border-gray-300 my-4" />
-        <CertificationsSection />
-      </div>
-    </div>
-  </Layout>
+    </Layout>
+  </MouseWrapper>
 );
