e154ba75adb44e556459aef0107da4ffc3d12058
Juho Lehtimäki <juho.a.lehtimaki@gmail.com>
2025-04-15 00:53:29 +0300

    fix: social media link cursor

--- apps/client/src/components/SocialMediaLink.tsx
+++ apps/client/src/components/SocialMediaLink.tsx
@@ -12,7 +12,11 @@ export const SocialMediaLink: React.FC<Props> = ({ icon: Icon, url }) => {
   };
 
   return (
-    <button type="button" onClick={handleClick} className="hover:text-blue-500">
+    <button
+      type="button"
+      onClick={handleClick}
+      className="hover:text-blue-500 cursor-pointer"
+    >
       <Icon size={25} className="text-gray-400 hover:text-gray-100" />
     </button>
   );
